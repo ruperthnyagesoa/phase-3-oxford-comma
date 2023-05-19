087773c38d98ca5467ea74be5875f025a9611000
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-05-19 03:08:36 +0300

    Completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  array.join(":-)")
+  case array.length
+  when 0
+    ""
+    when 1
+        array[0]
+    when 2
+        "#{array[0]} and #{array[1]}"
+    else
+        "#{array[0..-2].join(', ')}, and #{array[-1]}"
+ end
+ 
 end
